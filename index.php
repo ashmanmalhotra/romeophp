d0e1a7daa183b64360810c5602598e487e0c67b5
Ashman Malhotra <ashmanmalhotra@gmail.com>
2024-04-10 12:19:50 +0530

    file uploaded
    files uploaded

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'includes/header.php'; ?>
+<!-- Navigation-->
+<?php include 'includes/navigation.php'; ?>
+<!-- Page Header-->
+<?php include 'includes/page_header.php'; ?>
+<!-- Main Content-->
+<?php include 'includes/posts.php'; ?>
+<!-- Footer-->
+<?php include 'includes/footer.php'; ?>
